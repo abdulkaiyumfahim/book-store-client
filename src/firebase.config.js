3183b12683db3f1f08ba6e0cc8f1a1ae93bf5196
MdKaioumislam5 <afrankaiyum34@gmail.com>
2023-03-15 20:16:11 +0600

    finally work-firebase.envFile

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -4,23 +4,23 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey:"AIzaSyAyeeUIW-XfsDuNVCA4ezhs7Nd86YKWK3Q",
-  authDomain:"book-store-30692.firebaseapp.com",
-  projectId:"book-store-30692",
-  storageBucket:"book-store-30692.appspot.com",
-  messagingSenderId:"935317187114",
-  appId:"1:935317187114:web:e6b24a309f69d79f73b153"
-};
-
 // const firebaseConfig = {
-//   apiKey:process.env.REACT_APP_apiKey,
-//   authDomain:process.env.REACT_APP_authDomain,
-//   projectId:process.env.REACT_APP_projectId,
-//   storageBucket:process.env.REACT_APP_storageBucket,
-//   messagingSenderId:process.env.REACT_APP_messagingSenderId,
-//   appId:process.env.REACT_APP_appId,
+//   apiKey:"AIzaSyAyeeUIW-XfsDuNVCA4ezhs7Nd86YKWK3Q",
+//   authDomain:"book-store-30692.firebaseapp.com",
+//   projectId:"book-store-30692",
+//   storageBucket:"book-store-30692.appspot.com",
+//   messagingSenderId:"935317187114",
+//   appId:"1:935317187114:web:e6b24a309f69d79f73b153"
 // };
+
+const firebaseConfig = {
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
